04cbd9ca7c7f3ce8c4abd6ecdd88f6c6947fd08d
Leon Tan <tanxx519@umn.edu>
2018-10-31 23:36:53 -0500

    updating

--- 11-sql-alchemy-challenge/app_climate.py
+++ 11-sql-alchemy-challenge/app_climate.py
@@ -116,19 +116,5 @@ def start(start_date):
         start_dates.append(start_dict)
 
     return jsonify(start_dates)
-
-@app.route("/api/v1.0/justice-league/real_name/<real_name>")
-def justice_league_by_real_name(real_name):
-    """Fetch the Justice League character whose real_name matches
-       the path variable supplied by the user, or a 404 if not."""
-
-    canonicalized = real_name.replace(" ", "").lower()
-    for character in justice_league_members:
-        search_term = character["real_name"].replace(" ", "").lower()
-
-        if search_term == canonicalized:
-            return jsonify(character)
-
-    return jsonify({"error": f"Character with real_name {real_name} not found."}), 404
 if __name__ == '__main__':
     app.run(debug=True)
