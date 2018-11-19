12d6dc28a1589fab5359aceee1d18e4b203d9557
tanxx519 <tanxx519@umn.edu>
2018-11-18 21:03:06 -0600

    updated

---
+++ 11-web-scraping-challenge/config.py
@@ -0,0 +1,4 @@
+consumer_key = "DOoZ0lH8bnzzCb46YbDwCxTfm"
+consumer_secret = "YLGmzQGuEHnT0zrR4rlVr7FyISG2R3v1g1G4tq2jgXwddlbb1Q"
+access_token = "1038858933560115200-8sMRdSrSVnqPn8DAk9BG6j5my6SACl"
+access_token_secret = "2rORseStgLPSdvjOG06TKwTBc0SnFVjglmvdhXp26721a"
