8701fc5954df756e2b24e70f7591ee8ed86b3542
tanxx519 <tanxx519@umn.edu>
2018-12-09 21:26:58 -0600

    finished Step 1

--- 12-javascript-challenge/static/js/app.js
+++ 12-javascript-challenge/static/js/app.js
@@ -2,3 +2,17 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+// console.log(tableData);
+
+tableData.forEach(items => {
+    var row = d3.select("tbody").append("tr");
+
+    Object.entries(items).forEach(([key, value]) => {
+        var info = row.append("td");
+        info.text(value);
+    });
+});
+
+// var row = d3.select("tbody").append("tr");
+
+// var datas = row.append("td").text(dates);
